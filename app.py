d039efed54db4db27ebb55001e144e0a11a7e0f7
Adam Englander <adamenglander@yahoo.com>
2019-04-07 14:21:57 -0700

    Cleaned up and documented

--- app.py
+++ app.py
@@ -1,149 +1,157 @@
-import os, sqlite3
+import sqlite3
 
 import click
 
-from flask import Flask, current_app, g
+from flask import Flask, g
 from flask.cli import with_appcontext
 from flask_restplus import Api, Resource, fields
 
+# Create the Flask application
 app = Flask(__name__)
 
 
 def get_db():
+    """
+    Helper function to obtain a database connection from the global application
+    context.
+    :rtype: sqlite.Connection
+    """
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            'todo.sq3', detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db = sqlite3.connect('todo.sq3',
+                               detect_types=sqlite3.PARSE_DECLTYPES)
+
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
 
+# Define the function to close the database connection
 def close_db(_=None):
+    """
+    Remove the database connection from the global application context
+    and close the connection.
+    """
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
 
 
+# Add the close_db to the teardown of the app context tp ensure the
+# connection is closed when the Flask application shuts down.
+app.teardown_appcontext(close_db)
+
+
+# Add the command toi initialize the database
 @click.command('init-db')
 @with_appcontext
 def init_db():
-    """Create database schema if nin exists."""
-    get_db().execute("""
-        CREATE TABLE IF NOT EXISTS todo (
+    """Create database schema if none exists."""
+    get_db().execute(
+        """
+        CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            task TEXT)""")
+            detail TEXT)
+        """)
 
 
-app.teardown_appcontext(close_db)
+# Add the init-db command to the Flask application
 app.cli.add_command(init_db)
 
+# Create the Flask Rest+ API definition
 api = Api(app, version='1.0', title='To Do API',
           description='A example "To Do" API')
 
-ns = api.namespace('todos', description='To Do operations')
+# Create the tasks namespace /tasks
+ns = api.namespace('tasks', description='Task operations')
 
-todo = api.model('ToDo', {
-    'id': fields.Integer(readOnly=True,
-                         description='The task unique identifier'),
-    'task': fields.String(required=True, description='The task details')
-})
+# Create the model for displaying tasks
+task_show_model = api.model(
+    'Task Show', {
+        'id': fields.Integer(readOnly=True,
+                             description='The task unique identifier'),
+        'detail': fields.String(required=True, description='The task detail')
+    })
 
-todo_change = api.model('ToDo Change', {
-    'task': fields.String(required=True, description='The task details')
-})
+# Create the model for creating or updating tasks
+task_change_model = api.model(
+    'Task Change', {
+        'detail': fields.String(required=True, description='The task detail')
+    })
 
 
-class NotFoundException(Exception):
-    pass
+# Create the resource for the root of the tasks namespace /tasks/
+@ns.route('/')
+class Tasks(Resource):
+    """
+    Provides for management of the task resources as a whole such
+    as adding a new task or showing a complete list of tasks.
+    """
 
-
-class TodoDAO(object):
-
-    def all(self):
+    @ns.doc('list_tasks')
+    @ns.marshal_list_with(task_show_model)
+    def get(self):
+        """List all tasks"""
         rows = []
         with get_db() as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT id, task FROM todo')
+            cursor.execute('SELECT id, detail FROM tasks')
             for row in cursor:
-                rows.append({'id': row[0], 'task': row[1]})
-        return rows
+                rows.append({'id': row[0], 'detail': row[1]})
+        return rows, 200
 
-    def get(self, id):
-        with get_db() as conn:
-            cursor = conn.cursor()
-            cursor.execute('SELECT id, task FROM todo where id=?', (id, ))
-            row = cursor.fetchone()
-            if row is None:
-                api.abort(404, "Todo {} doesn't exist".format(id))
-            return {'id': row[0], 'task': row[1]}
-
-    def create(self, data):
-        with get_db() as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'INSERT INTO todo (task) VALUES (?)',
-                (data['task'],))
-            data['id'] = cursor.lastrowid
-        return data
-
-    def update(self, id, data):
-        with get_db() as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'REPLACE INTO todo (id, task) VALUES (?, ?)',
-                (id, data['task']))
-        data['id'] = id
-        return data
-
-    def delete(self, id):
-        with get_db() as conn:
-            cursor = conn.cursor()
-            cursor.execute('DELETE FROM todo WHERE id=?', (id,))
-
-
-DAO = TodoDAO()
-
-
-@ns.route('/')
-class TodoList(Resource):
-    """Shows a list of all todos, and lets you POST to add new tasks"""
-
-    @ns.doc('list_todos')
-    @ns.marshal_list_with(todo)
-    def get(self):
-        """List all tasks"""
-        return TodoDAO().all(), 200
-
-    @ns.doc('create_todo')
-    @ns.expect(todo_change)
-    @ns.marshal_with(todo, code=201)
+    @ns.doc('create_task')
+    @ns.expect(task_change_model, validate=True)
+    @ns.marshal_with(task_show_model, code=201)
     def post(self):
         """Create a new task"""
-        return TodoDAO().create(api.payload), 201
+        data = api.payload
+        with get_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                'INSERT INTO tasks (detail) VALUES (?)',
+                (data['detail'],))
+            data['id'] = cursor.lastrowid
+        return data, 201
+
 
 
 @ns.route('/<int:id>')
 @ns.param('id', 'The task identifier')
-class Todo(Resource):
-    """Show a single todo item and lets you delete them"""
+class Task(Resource):
+    """Interact with a particular task item"""
 
-    @ns.doc('get_todo')
-    @ns.marshal_with(todo)
-    @ns.response(404, 'Todo not found')
+    @ns.doc('get_task')
+    @ns.marshal_with(task_show_model)
+    @ns.response(404, 'Task not found')
     def get(self, id):
-        """Fetch a given resource"""
-        return TodoDAO().get(id)
+        """Fetch a task"""
+        with get_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT id, detail FROM tasks where id=?', (id,))
+            row = cursor.fetchone()
+            if row is None:
+                api.abort(404, "Task {} doesn't exist".format(id))
+            return {'id': row[0], 'detail': row[1]}
 
-    @ns.doc('delete_todo')
-    @ns.response(204, 'Todo deleted')
+    @ns.doc('delete_task')
+    @ns.response(204, 'Task deleted')
     def delete(self, id):
-        """Delete a task given its identifier"""
-        TodoDAO().delete(id)
+        """Delete a task"""
+        with get_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute('DELETE FROM tasks WHERE id=?', (id,))
         return None, 204
 
-    @ns.expect(todo_change)
-    @ns.marshal_with(todo)
+    @ns.expect(task_change_model, validate=True)
+    @ns.marshal_with(task_show_model)
     def put(self, id):
-        """Update a task given its identifier"""
-        return TodoDAO().update(id, api.payload)
+        """Set a task"""
+        data = api.payload
+        with get_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                'REPLACE INTO tasks (id, detail) VALUES (?, ?)',
+                (id, data['detail']))
+        data['id'] = id
+        return data
